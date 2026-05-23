20e3493c934d1499c14c22c8ce4e4e9c4812b8d6
lokcode <alok41n42@gmail.com>
2026-05-23 23:06:23 +0530

    render url fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const { scoreAllClaims } = require("./scorer");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
